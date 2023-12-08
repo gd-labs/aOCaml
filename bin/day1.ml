a2f7dfe62837f48f56ec8688a3df18de85cbe46c
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-12-08 10:32:16 -0300

    style: formatting

--- bin/day1.ml
+++ bin/day1.ml
@@ -3,25 +3,27 @@ open List
 
 let part_one lines =
   fold lines ~init:0 ~f:(fun acc line ->
-    let nums = 
+    let nums =
       line
       |> String.to_list
       |> filter_map ~f:(fun ch ->
-        if Char.is_digit ch then (Char.get_digit ch) else None)
+        if Char.is_digit ch then Char.get_digit ch else None)
     in
-    acc + (hd_exn nums) * 10 + (last_exn nums))
+    acc + (hd_exn nums * 10) + last_exn nums)
 ;;
 
 let cases =
-[ "one", 1
-; "two", 2
-; "three", 3
-; "four", 4
-; "five", 5
-; "six", 6
-; "seven", 7
-; "eight", 8
-; "nine", 9 ]
+  [ "one", 1
+  ; "two", 2
+  ; "three", 3
+  ; "four", 4
+  ; "five", 5
+  ; "six", 6
+  ; "seven", 7
+  ; "eight", 8
+  ; "nine", 9
+  ]
+;;
 
 let part_two lines =
   fold lines ~init:0 ~f:(fun acc line ->
@@ -29,19 +31,17 @@ let part_two lines =
       line
       |> String.foldi ~init:[] ~f:(fun pos acc ch ->
         let num =
-          match ch with
-          | ch when Char.is_digit ch -> Char.get_digit ch
-          | _ -> (
+          if Char.is_digit ch then Char.get_digit
+          else
             find_map cases ~f:(fun (substr, value) ->
-              match String.substr_index ~pos:pos line ~pattern:substr with
+              match String.substr_index ~pos line ~pattern:substr with
               | Some matched when matched = pos -> Some value
               | _ -> None)
-          )
         in
-        acc @ [num])
+        acc @ [ num ])
       |> filter_opt
     in
-    acc + (hd_exn nums) * 10 + (last_exn nums))
+    acc + (hd_exn nums * 10) + last_exn nums)
 ;;
 
 let () =
